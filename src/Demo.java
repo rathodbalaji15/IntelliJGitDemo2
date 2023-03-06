8cbf7fda14177744de1aaf47ad7deea38c9e51b4
balajirathod <rathodbalaji123@gmail.com>
2023-03-06 15:04:11 +0530

    Practice with InteliiJ for commit

---
+++ src/Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Sample code for commit");
+        System.out.println("Second line for commit");
+    }
+}
