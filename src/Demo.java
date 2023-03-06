43cfa72819f169f60ed88fe81a01aa25a0b1cc21
balajirathod <rathodbalaji123@gmail.com>
2023-03-06 15:11:06 +0530

    Third line added

--- src/Demo.java
+++ src/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Sample code for commit");
         System.out.println("Second line for commit");
+        System.out.println("Third line for commit");
     }
 }
